ee72ea60bf392893141b21b9d1f0c810668ebd4c
Teraz6 <retenk@gmail.com>
2025-09-22 16:20:57 +0300

    Update PlanetsPage

--- MauiPlanets/MauiPlanets/Views/PlanetsPage.xaml.cs
+++ MauiPlanets/MauiPlanets/Views/PlanetsPage.xaml.cs
@@ -1,11 +1,28 @@
+using MauiPlanets.Services;
 
 namespace MauiPlanets.Views;
 
 public partial class PlanetsPage : ContentPage
 {
+	private const uint AnimationDuration = 800u;
+
 	public PlanetsPage()
 	{
 		InitializeComponent();
 	}
 
+    protected override void OnAppearing()
+    {
+        base.OnAppearing();
+
+		//lstPopularPlanets.ItemsSource = PlanetsService.getFeaturedPlanets();
+		//lstAllPlanets.ItemsSource = PlanetsService.GetAllPlanets();
+    }
+
+    async void ProfilePic_Clicked(System.Object sender, System.EventArgs e)
+    {
+        _ = MainContentGrid.TranslateTo(-this.Width * 0.5, this.Height * 0.1, AnimationDuration, Easing.CubicIn);
+        await MainContentGrid.ScaleTo(0.8, AnimationDuration);
+        _ = MainContentGrid.ScaleTo(0.8, AnimationDuration);
+    }
 }
